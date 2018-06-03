a3958885ce32cf0bc7d9d8d29138c26775a18e58
Alexander James Phillips <ajp.global@gmail.com>
2018-06-03 17:12:38 +0100

    Add log_network_wifi_stats to stats reporter

--- src/stat_reporter/stat_reporter.py
+++ src/stat_reporter/stat_reporter.py
@@ -24,7 +24,7 @@ from utils.intervaled_ma import intervaled_ma
 log = logger_factory('stat_reporter')
 
 
-def log_network_stats(log, config, now):
+def log_network_segment_stats(log, config, now):
     try:
         process = subprocess.Popen(
             args=["netstat", "-s"],
@@ -79,6 +79,62 @@ def log_network_stats(log, config, now):
         return []
 
 
+def log_network_wifi_stats(log, config, now):
+    """
+    TODO MAYBE?  use output from: `cat /proc/net/wireless`:
+    Inter-| sta-|   Quality        |   Discarded packets               | Missed | WE
+    face | tus | link level noise |  nwid  crypt   frag  retry   misc | beacon | 22
+    wlan0: 0000   55.  -55.  -256        0      0      0     85      0        0
+    """
+    try:
+        process = subprocess.Popen(
+            args=["iwconfig", "wlan0"],
+            shell=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        """
+        wlan0     IEEE 802.11  ESSID:"VM1557216"
+                Mode:Managed  Frequency:2.412 GHz  Access Point: C0:05:C2:3A:3B:89
+                Bit Rate=72.2 Mb/s   Tx-Power=31 dBm
+                Retry short limit:7   RTS thr:off   Fragment thr:off
+                Power Management:on
+                Link Quality=53/70  Signal level=-57 dBm
+                Rx invalid nwid:0  Rx invalid crypt:0  Rx invalid frag:0
+                Tx excessive retries:84  Invalid misc:0   Missed beacon:0
+        """
+        text = process.communicate()[0].decode("utf-8")
+        batched = []
+        node_name = config["node_name"]
+
+        match = re.match('.*Link Quality=(\d+)/(\d+)  Signal level=([^ dBm]+).*', text, re.DOTALL)
+        quality = int(match.group(1))
+        quality_total = int(match.group(2))
+        metric_type = "stats-link_quality"
+        data = {
+            "type": metric_type,
+            "datetime": now,
+            "tags": [node_name + " " + metric_type],
+            "value": quality / quality_total,
+        }
+        batched.append(data)
+
+        signal_level = int(match.group(3))
+        metric_type = "stats-signal_level"
+        data = {
+            "type": metric_type,
+            "datetime": now,
+            "tags": [node_name + " " + metric_type],
+            "value": signal_level,
+        }
+        batched.append(data)
+
+        return batched
+    except Exception as e:
+        log.exception("Reading from iwconfig received error: \"{}\"".format(e))
+        return []
+
+
 def action_func_factory(log, config):
 
     log.info('Got config: {}'.format(config["stat_reporter"]))
@@ -87,7 +143,8 @@ def action_func_factory(log, config):
 
         stats = []
         if config["stat_reporter"]["enabled"]:
-            stats = stats + log_network_stats(log=log, config=config, now=now)
+            stats = stats + log_network_segment_stats(log=log, config=config, now=now)
+            stats = stats + log_network_wifi_stats(log=log, config=config, now=now)
         return stats
 
     return action_func
